f6dafec81e09b79f6b503bd7d3bc2288ca55944e
sara <sara.wagner23q@gmail.com>
2016-01-22 09:01:23 -0800

    modified third test with numbers divisble by 5, first fail before code fix

--- specs/specs.js
+++ specs/specs.js
@@ -2,11 +2,11 @@ describe('pingPong', function() {
   it("it will count up to a number by one", function() {
     expect(pingPong(7)).to.eql([1, 2, 3, 4, 5, 6, 7]);
   });
-  it("it will give replace numbers divisble by 15 from a user number with ping-pong", function() {
+  it("it will give replace numbers divisble by 15 from a user number with the word ping-pong", function() {
     expect(pingPong(60)).to.eql("ping-pong");
   });
-  it("it will give numbers divisble by 5 from a user number", function() {
-    expect(pingPong(20)).to.eql([5, 10, 15, 20]);
+  it("it will give replace numbers divisble by 5 from a user number with the word pong", function() {
+    expect(pingPong(20)).to.eql("pong");
   });
   it("it will give numbers divisible by 3 from a user number", function () {
     expect(pingPong(12)).to.eql([3, 6, 9, 12]);
