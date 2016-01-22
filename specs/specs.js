e6faf62e9acf6bec3028281038313453baabde75
sara <sara.wagner23q@gmail.com>
2016-01-22 08:57:57 -0800

    modified second test to replace divisble by 15 with a word

--- specs/specs.js
+++ specs/specs.js
@@ -2,8 +2,8 @@ describe('pingPong', function() {
   it("it will count up to a number by one", function() {
     expect(pingPong(7)).to.eql([1, 2, 3, 4, 5, 6, 7]);
   });
-  it("it will give numbers divisble by 15 from a user number", function() {
-    expect(pingPong(60)).to.eql([15, 30, 45, 60]);
+  it("it will give replace numbers divisble by 15 from a user number with ping-pong", function() {
+    expect(pingPong(60)).to.eql("ping-pong");
   });
   it("it will give numbers divisble by 5 from a user number", function() {
     expect(pingPong(20)).to.eql([5, 10, 15, 20]);
