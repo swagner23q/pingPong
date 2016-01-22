b6cc62739848eceec9eedc93de8694d677539a6e
sara <sara.wagner23q@gmail.com>
2016-01-22 09:22:23 -0800

    begin styling and input javascript code into index.html

--- js/scripts.js
+++ js/scripts.js
@@ -7,27 +7,8 @@ var pingPong = function(userNumber) {
       return ("pong");
 
     } else if (userNumber % 3 === 0) {
-      return ([3, 6, 9, 12]);
+      return ("ping");
     }
     return ([1, 2, 3, 4, 5, 6, 7]);
   }
 }
-
-
-
-
-
-
-// var pingPong = function(userNumber) {
-//   for (var i = 1; i <= userNumber; i ++) {
-//     if (userNumber % 3 === 0) {
-//       return ([3, 6, 9, 12]);
-//
-//     } else if (userNumber % 5 === 0) {
-//       return ([5, 10, 15, 20]);
-//     } else if (userNumber % 15 ===15) {
-//       return ([15, 30, 45, 60]);
-//     }
-//     return ([1, 2, 3, 4, 5, 6, 7]);
-//   }
-// }
