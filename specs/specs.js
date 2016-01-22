8cfb6c988ab2cb9e6f0ce79fc22c698b3a98b4b4
sara <sara.wagner23q@gmail.com>
2016-01-22 09:04:21 -0800

    modified fourth test with numbers divisible by 3-replace with ping, first fail

--- specs/specs.js
+++ specs/specs.js
@@ -2,13 +2,13 @@ describe('pingPong', function() {
   it("it will count up to a number by one", function() {
     expect(pingPong(7)).to.eql([1, 2, 3, 4, 5, 6, 7]);
   });
-  it("it will give replace numbers divisble by 15 from a user number with the word ping-pong", function() {
+  it("it will replace numbers divisble by 15 from a user number with the word ping-pong", function() {
     expect(pingPong(60)).to.eql("ping-pong");
   });
-  it("it will give replace numbers divisble by 5 from a user number with the word pong", function() {
+  it("it will replace numbers divisble by 5 from a user number with the word pong", function() {
     expect(pingPong(20)).to.eql("pong");
   });
-  it("it will give numbers divisible by 3 from a user number", function () {
-    expect(pingPong(12)).to.eql([3, 6, 9, 12]);
+  it("it will replace numbers divisible by 3 from a user number with the word ping", function () {
+    expect(pingPong(12)).to.eql("ping");
   })
 });
