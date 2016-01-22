ace5ff254b856c80e4036742e1d342e8c9417684
sara <sara.wagner23q@gmail.com>
2016-01-22 08:25:36 -0800

    add if statement for numbers divisble by 3, passing second test

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 var pingPong = function(userNumber) {
   for (var i = 1; i <= userNumber; i ++) {
+    if (userNumber % 3 == 0) {
+      return ([3, 6, 9, 12])
+    }
     return ([1, 2, 3, 4, 5]);
   }
 }
