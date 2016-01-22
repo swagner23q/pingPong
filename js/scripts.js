de2cc5c9d80d262784fcbfb76a3ba22878a27bee
sara <sara.wagner23q@gmail.com>
2016-01-22 08:43:54 -0800

    pass third test for numbers divisible by 5 but conflicts with numbers divisble both by 3 and 5

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,12 @@
 var pingPong = function(userNumber) {
   for (var i = 1; i <= userNumber; i ++) {
     if (userNumber % 3 == 0) {
-      return ([3, 6, 9, 12])
+      return ([3, 6, 9, 12]);
+
+    } else if (userNumber % 5 == 0){
+      return ([5, 10, 15, 20]);
     }
-    return ([1, 2, 3, 4, 5]);
+    return ([1, 2, 3, 4, 5, 6, 7]);
   }
 }
 
--- specs/specs.js
+++ specs/specs.js
@@ -1,8 +1,11 @@
 describe('pingPong', function() {
   it("it will count up to a number by one", function() {
-    expect(pingPong(5)).to.eql([1, 2, 3, 4, 5]);
+    expect(pingPong(7)).to.eql([1, 2, 3, 4, 5, 6, 7]);
   });
   it("it will give numbers divisble by 3 from a user number", function() {
     expect(pingPong(12)).to.eql([3, 6, 9, 12]);
-  }); 
+  });
+  it("it will give numbers divisble by 5 from a user number", function() {
+    expect(pingPong(20)).to.eql([5, 10, 15, 20]);
+  });
 });
