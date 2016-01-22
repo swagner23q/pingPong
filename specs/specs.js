2c0a88514e65a363dd8ce8b3bc4561e85b49268d
sara <sara.wagner23q@gmail.com>
2016-01-22 08:17:29 -0800

    create second test to output numbers divisible by 3

--- specs/specs.js
+++ specs/specs.js
@@ -2,4 +2,7 @@ describe('pingPong', function() {
   it("it will count up to a number by one", function() {
     expect(pingPong(5)).to.eql([1, 2, 3, 4, 5]);
   });
+  it("it will give numbers divisble by 3 from a user number", function() {
+    expect(pingPong(12)).to.eql([3, 6, 9, 12]);
+  }); 
 });
