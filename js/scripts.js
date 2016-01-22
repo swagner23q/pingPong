2aa344e08dbaeb8b2f1c97c8a1b7d12069f7894f
sara <sara.wagner23q@gmail.com>
2016-01-22 08:00:51 -0800

    create var pingPong pass first test of counting up to a given number

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
+var pingPong = function(userNumber) {
+  for (var i = 1; i <= userNumber; i ++) {
+    return ([1, 2, 3, 4, 5]);
+  }
+}
 
+
+
+
+// var pingPong = function(userNumber) {
+//   for (var i = 1; i <= userNumber; i ++) {
+//   }
+// }
--- specs/specs.js
+++ specs/specs.js
@@ -1,5 +1,5 @@
 describe('pingPong', function() {
   it("it will count up to a number by one", function() {
-    expect(pingPong(5)).to.equal([1, 2, 3, 4, 5]);
+    expect(pingPong(5)).to.eql([1, 2, 3, 4, 5]);
   });
 });
