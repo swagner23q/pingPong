58950b5428d33e238ae9df72ae621d53bbd21a99
sara <sara.wagner23q@gmail.com>
2016-01-22 13:52:10 -0800

    show results correctly with pingpong while column styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,63 +1,32 @@
-// var pingPong = function(userNumber) {
-//   for (var i = 1; i <= userNumber; i ++) {
-//     if (userNumber % 15 === 0) {
-//       $("ul#pingPongpang").append("<li>ping-pong</li>");
-//
-//     } else if (userNumber % 5 === 0) {
-//       $("ul#pingPongpang").append("<li>pong</li>");
-//
-//     } else if (userNumber % 3 === 0) {
-//       $("ul#pingPongpang").append("<li>ping</li>");
-//     }
-//     return $("ul#pingPongpang").append("<li></li>");
-//   }
-// }
 var pingPong = function(userNumber) {
   var outcome = [];
-  for (var i = 1; i <= userNumber; i ++) {
-    outcome.push(i);
-  }
-    if (userNumber % 15 === 0) {
-      return (outcome.push(i));
-    }
-    //
-    // } else if (userNumber % 5 === 0) {
-    //   return (result + (result.replace("pong")));
-    //
-    // } else if (userNumber % 3 === 0) {
-    //   return ("ping");
-    //
-    // } else {
-    //   }
-    return outcome;
-}
 
+  // if (isNan(userNumber)) {
+  //   alert("Please enter a numeric value, thx.")
+  // }
+
+  for (var i = 1; i <= userNumber; i ++) {
+    if (i % 15 === 0) {
+      outcome.push("ping-pong");
+    } else if (i % 5 === 0) {
+      outcome.push("pong");
+    } else if (i % 3 === 0) {
+      outcome.push("ping");
+    } else {
+      outcome.push(i);
+    }
+  }
+  return outcome;
+};
 
 $(document).ready(function() {
   $("button#pongMe").click(function(event) {
-    debugger;
+    $("#pingPongpang" ).empty();
     var userNumber = parseFloat($("input#userNum").val());
     var results = pingPong(userNumber);
 
-    $("#pingPongpang").text(results);
+    $("#pingPongpang").append("<li>" + results + "</li>");
 
     event.preventDefault();
   });
 });
-
-// var pingPong = function(userNumber) {
-//   for (var i = 1; i <= userNumber; i ++) {
-//     if (userNumber % 15 === 0) {
-//       return ("ping-pong");
-//
-//     } else if (userNumber % 5 === 0) {
-//       return ("pong");
-//
-//     } else if (userNumber % 3 === 0) {
-//       return ("ping");
-//     }
-//     return ([1, 2, 3, 4, 5, 6, 7]);
-//   }
-// }
-// (isNaN(userNumber)) {
-//   return false;
