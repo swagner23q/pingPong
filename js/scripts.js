91d925568b5100cbe5703e995a5d031c1a70ecf8
sara <sara.wagner23q@gmail.com>
2016-01-22 10:27:00 -0800

    plugged in pingPong variable to index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,41 @@
 var pingPong = function(userNumber) {
   for (var i = 1; i <= userNumber; i ++) {
     if (userNumber % 15 === 0) {
-      return ("ping-pong");
+      $("ul#pingPongpang").append("<li>ping-pong</li>");
 
     } else if (userNumber % 5 === 0) {
-      return ("pong");
+      $("ul#pingPongpang").append("<li>pong</li>");
 
     } else if (userNumber % 3 === 0) {
-      return ("ping");
+      $("ul#pingPongpang").append("<li>ping</li>");
     }
-    return ([1, 2, 3, 4, 5, 6, 7]);
+    return $("ul#pingPongpang").append("<li></li>");
   }
 }
-// $(document).ready(function() {
-//   $("form#pongPlatform").submit(function(event) {
-//     var userNumber = parseFloat($("input#userNum").val());
+$(document).ready(function() {
+  $("button#pongMe").click(function(event) {
+
+    var userNumber = parseFloat($("input#userNum").val());
+    var results = pingPong(userNumber);
+
+    $("#pingPongpang").text(results);
+    event.preventDefault();
+  });
+});
+
+// var pingPong = function(userNumber) {
+//   for (var i = 1; i <= userNumber; i ++) {
+//     if (userNumber % 15 === 0) {
+//       return ("ping-pong");
 //
+//     } else if (userNumber % 5 === 0) {
+//       return ("pong");
 //
-//
-//
-//     event.preventDefault();
-//   });
-// });
+//     } else if (userNumber % 3 === 0) {
+//       return ("ping");
+//     }
+//     return ([1, 2, 3, 4, 5, 6, 7]);
+//   }
+// }
+// (isNaN(userNumber)) {
+//   return false;
--- specs/specs.js
+++ specs/specs.js
@@ -1,17 +1,21 @@
 describe('pingPong', function() {
+  // it("it will return false if values entered are not numbers", function () {
+  // expect(pingPong("word")).to.equal(false);
+  // });
+
   it("it will count up to a number by one", function() {
     expect(pingPong(7)).to.eql([1, 2, 3, 4, 5, 6, 7]);
   });
+
   it("it will replace numbers divisble by 15 from a user number with the word ping-pong", function() {
     expect(pingPong(60)).to.equal("ping-pong");
   });
+
   it("it will replace numbers divisble by 5 from a user number with the word pong", function() {
     expect(pingPong(20)).to.equal("pong");
   });
+
   it("it will replace numbers divisible by 3 from a user number with the word ping", function () {
     expect(pingPong(12)).to.equal("ping");
-  })
-  it("it will return false when not a number", function () {
-    expect(pingPong("word").to.equal(false))
-  })
+  });
 });
