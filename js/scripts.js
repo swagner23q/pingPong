4be1eb6423c3ab97349a527b64db313aefd4e347
sara <sara.wagner23q@gmail.com>
2016-01-22 08:52:50 -0800

    pass fourth test by switching logic positions of 3 and 15

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,13 @@
 var pingPong = function(userNumber) {
   for (var i = 1; i <= userNumber; i ++) {
-    if (userNumber % 3 == 0) {
-      return ([3, 6, 9, 12]);
+    if (userNumber % 15 === 0) {
+      return ([15, 30, 45, 60]);
 
-    } else if (userNumber % 5 == 0){
+    } else if (userNumber % 5 === 0) {
       return ([5, 10, 15, 20]);
+      
+    } else if (userNumber % 3 === 0) {
+      return ([3, 6, 9, 12]);
     }
     return ([1, 2, 3, 4, 5, 6, 7]);
   }
@@ -13,7 +16,18 @@ var pingPong = function(userNumber) {
 
 
 
+
+
 // var pingPong = function(userNumber) {
 //   for (var i = 1; i <= userNumber; i ++) {
+//     if (userNumber % 3 === 0) {
+//       return ([3, 6, 9, 12]);
+//
+//     } else if (userNumber % 5 === 0) {
+//       return ([5, 10, 15, 20]);
+//     } else if (userNumber % 15 ===15) {
+//       return ([15, 30, 45, 60]);
+//     }
+//     return ([1, 2, 3, 4, 5, 6, 7]);
 //   }
 // }
--- specs/specs.js
+++ specs/specs.js
@@ -2,10 +2,13 @@ describe('pingPong', function() {
   it("it will count up to a number by one", function() {
     expect(pingPong(7)).to.eql([1, 2, 3, 4, 5, 6, 7]);
   });
-  it("it will give numbers divisble by 3 from a user number", function() {
-    expect(pingPong(12)).to.eql([3, 6, 9, 12]);
+  it("it will give numbers divisble by 15 from a user number", function() {
+    expect(pingPong(60)).to.eql([15, 30, 45, 60]);
   });
   it("it will give numbers divisble by 5 from a user number", function() {
     expect(pingPong(20)).to.eql([5, 10, 15, 20]);
   });
+  it("it will give numbers divisible by 3 from a user number", function () {
+    expect(pingPong(12)).to.eql([3, 6, 9, 12]);
+  })
 });
